77191019b27b992763a2008ddb45a1f24e819211
ramonmv <raamon@gmail.com>
2020-08-28 05:31:46 -0300

    Debate config + login.php - teste de conexao

--- login.php
+++ login.php
@@ -1,5 +1,120 @@
 <?php
 
-phpinfo();
+// phpinfo();
+
+// MYSQL_VERSION=latest
+// MYSQL_DATABASE=default
+// MYSQL_USER=ramon
+// MYSQL_PASSWORD=password
+// MYSQL_PORT=3306
+// MYSQL_ROOT_PASSWORD=amadis
+// MYSQL_ENTRYPOINT_INITDB=./mysql/docker-entrypoint-initdb.d
+
+
+$link = mysqli_connect("localhost", "ramon", "password", "debate");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    // exit;
+}
+else{
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+}
+
+mysqli_close($link);
+
+
+
+
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$link = mysqli_connect("localhost", "ramon", "amadis", "debate");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    // exit;
+}
+
+else{
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+    
+}
+
+mysqli_close($link);
+
+
+
+
+
+
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$link = mysqli_connect("localhost", "root", "password", "debate");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    // exit;
+}
+
+else{
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+    
+}
+
+mysqli_close($link);
+
+
+
+
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$link = mysqli_connect("localhost", "root", "amadis", "debate");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    // exit;
+}
+
+else{
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+    
+}
+
+mysqli_close($link);
+
+
+
+
+
+
 
 ?>
